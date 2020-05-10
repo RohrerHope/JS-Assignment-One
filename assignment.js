42e5f4036bb2aba1ca0bc885e10727d9387e58e1
Charles <crohrerv@gmail.com>
2020-05-10 10:04:04 +0000

    Assignment completed

---
+++ assignment.js
@@ -0,0 +1,14 @@
+const fictionalNumber = 41;
+let result;
+
+result = fictionalNumber + 18;
+
+result = result - 13;
+
+result = result * 8;
+
+result = result / 2;
+
+alert(fictionalNumber);
+
+alert(result);
